852047da3edd65c4f682472606cc24cfe9d98b34
ye0nj.kim <fulmeach787@gmail.com>
2024-07-03 10:59:26 +0900

    page 38

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -4,14 +4,18 @@
 #include <vector>
 using namespace std;
 
-TEST(PrimeFactors, Of1) {
+class PrimeFixture : public testing::Test {
+public:
 	PrimeFactor prime_factor;
+	vector<int> expected;
+};
+
+TEST_F(PrimeFixture, Of1) {
 	vector<int> expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
 }
 
-TEST(PrimeFactors, Of2) {
-	PrimeFactor prime_factor;
+TEST_F(PrimeFixture, Of2) {
 	vector<int> expected = {2};
 	EXPECT_EQ(expected, prime_factor.of(2));
 }
