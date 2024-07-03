8e8695330951505cb1e20e88ac525c2691823768
ye0nj.kim <fulmeach787@gmail.com>
2024-07-03 11:02:25 +0900

    page 44

--- Project10/prime-factors.cpp
+++ Project10/prime-factors.cpp
@@ -5,11 +5,14 @@ class PrimeFactor {
 public:
 	vector<int> of(int number) {
 		vector<int> result = {};
-		if (number == 2) {
-			result.push_back(2);
-		}
-		if (number == 3) {
-			result.push_back(3);
+		if (number >  1) {
+			if (number == 4) {
+				result.push_back(2);
+				result.push_back(2);
+			}
+			else {
+				result.push_back(number);
+			}
 		}
 		return result;
 	}
