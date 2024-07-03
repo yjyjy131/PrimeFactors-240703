948b99c7e9fa3a6f64a25544cccca6cbe7e79ae5
ye0nj.kim <fulmeach787@gmail.com>
2024-07-03 10:56:39 +0900

    page 33

--- Project10/prime-factors.cpp
+++ Project10/prime-factors.cpp
@@ -1,5 +1,10 @@
+#include <vector>
+using namespace std;
 
 class PrimeFactor {
-
+public:
+	vector<int> of(int i) {
+		return {};
+	}
 };
 
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,9 +1,11 @@
 #include "pch.h"
 #include "../Project10/prime-factors.cpp"
 
+#include <vector>
 using namespace std;
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(PrimeFactors, Of1) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {};
+	EXPECT_EQ(expected, prime_factor.of(1));
 }
